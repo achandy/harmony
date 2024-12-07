03af5b55ac3af1e02fb8bdfcb2bfbd59f9c4c268
achandy <allan.chandy@yahoo.com>
2024-12-07 14:55:25 -0600

    Update main to use search instead of artist id

--- src/main.py
+++ src/main.py
@@ -1,25 +1,28 @@
-# This is a sample Python script.
-
 from spotify.spotify_client import SpotifyClient
 
 
-def print_artist(access_token, artist_id):
+def print_artist(access_token, artist_name):
     # Initialize the Spotify client with the provided access token
     spotify_client = SpotifyClient(access_token=access_token)
 
-    # Fetch artist details using the client
-    artist_details = spotify_client.fetch_artist(artist_id=artist_id)
+    # Search for the artist by name
+    search_results = spotify_client.search(query=artist_name, type='artist')
 
-    # Print the artist's details
-    print(f"Artist Name: {artist_details['name']}")
-    print(f"Genres: {', '.join(artist_details['genres'])}")
-    print(f"Popularity: {artist_details['popularity']}")
-    print(f"Followers: {artist_details['followers']['total']}")
+    # Retrieve the first artist from the search results
+    if search_results['artists']['items']:
+        artist_details = search_results['artists']['items'][0]
+
+        # Print the artist's details
+        print(f"Artist Name: {artist_details['name']}")
+        print(f"Genres: {', '.join(artist_details['genres'])}")
+        print(f"Popularity: {artist_details['popularity']}")
+        print(f"Followers: {artist_details['followers']['total']}")
+    else:
+        print(f"No artists found with the name: {artist_name}")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    # Replace 'your_access_token' and 'artist_id' with actual values
-    print_artist(access_token='', artist_id='4Z8W4fKeB5YxbusRsdQVPb')
+    # Replace 'your_access_token' and 'artist_name_here' with actual values
+    print_artist(access_token='your_access_token_here', artist_name='Drake')
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
