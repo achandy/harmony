f140e9689cd5e89723d296d7a968bbe19ad22ade
achandy <allan.chandy@yahoo.com>
2024-12-07 14:50:48 -0600

    Try out spotify client in main

--- src/main.py
+++ src/main.py
@@ -1,16 +1,25 @@
 # This is a sample Python script.
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from spotify.spotify_client import SpotifyClient
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+def print_artist(access_token, artist_id):
+    # Initialize the Spotify client with the provided access token
+    spotify_client = SpotifyClient(access_token=access_token)
+
+    # Fetch artist details using the client
+    artist_details = spotify_client.fetch_artist(artist_id=artist_id)
+
+    # Print the artist's details
+    print(f"Artist Name: {artist_details['name']}")
+    print(f"Genres: {', '.join(artist_details['genres'])}")
+    print(f"Popularity: {artist_details['popularity']}")
+    print(f"Followers: {artist_details['followers']['total']}")
 
 
 # Press the green button in the gutter to run the script.
 if __name__ == '__main__':
-    print_hi('PyCharm')
+    # Replace 'your_access_token' and 'artist_id' with actual values
+    print_artist(access_token='', artist_id='4Z8W4fKeB5YxbusRsdQVPb')
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
